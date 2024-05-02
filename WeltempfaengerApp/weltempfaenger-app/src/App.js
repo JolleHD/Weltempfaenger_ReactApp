7555043efbf277edc45e30a70d70d86a41047ffd
simplyhenri <yavexfx@gmail.com>
2024-05-02 14:10:25 +0200

    Noch ein Test
    Cool

--- WeltempfaengerApp/weltempfaenger-app/src/App.js
+++ WeltempfaengerApp/weltempfaenger-app/src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         TestRadioBranch
+        noch eine coole Zeile
         <a
           className="App-link"
           href="https://reactjs.org"

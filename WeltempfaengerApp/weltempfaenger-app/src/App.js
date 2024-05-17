2a5839fd3871a30c038bea1171326bfac12a98d3
YanCP <168456218+Yan73Zhang@users.noreply.github.com>
2024-05-17 14:08:03 +0200

    Codes neue Hinzufügen

--- WeltempfaengerApp/weltempfaenger-app/src/App.js
+++ WeltempfaengerApp/weltempfaenger-app/src/App.js
@@ -1,7 +1,15 @@
+import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+function App() { 
+  const title = "React";
+  return (
+
+    <div>
+      <h1 className="App">Hello {title}</h1>
+      
+    </div>
+  )
 }
 
 export default App;

29e49b15466f1cb4f3c5d42a9e160d72bd978782
YanCP <168456218+Yan73Zhang@users.noreply.github.com>
2024-05-17 13:48:49 +0200

    etwas Codes hin

--- WeltempfaengerApp/weltempfaenger-app/src/Weltkarte/MapApi.js
+++ WeltempfaengerApp/weltempfaenger-app/src/Weltkarte/MapApi.js
@@ -1,1 +1,23 @@
 import React from "react"; 
+const MapApi = () => {
+    const [currentStation, setCurrentStation] = useState(null);
+  
+    const stations = [
+      {
+        id: 1,
+        name: '1Live',
+        lat: 51.2266,
+        lng: 6.7782,
+        url: 'https://wdr-1live-live.icecast.wdr.de/wdr/1live/live/mp3/128/stream.mp3',
+      },
+      {
+        id: 2,
+        name: 'Radio Rur',
+        lat: 50.8072,
+        lng: 6.2824,
+        url: 'http://streams.radiork.de/radiorur',
+      },
+      // Weitere Stationen mit Koordinaten
+    ];
+  
+  

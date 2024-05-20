b2c9aa6f085df942d08a5202a20cc7661a1a3297
YanCP <168456218+Yan73Zhang@users.noreply.github.com>
2024-05-20 14:35:50 +0200

    Update App.js

--- WeltempfaengerApp/weltempfaenger-app/src/App.js
+++ WeltempfaengerApp/weltempfaenger-app/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
-import Weltkarte from "./Weltkarte/Weltkarte"
-import Radio from "./Weltkarte/Radio";
+//import Weltkarte from "./Weltkarte/Weltkarte"
 import RadioKarte from "./Weltkarte/RadioKarte";
 
 function App() { 
